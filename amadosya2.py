18faf08a2bbbc6a2847e31aad2e926d774470890
BULUT_2 <bulutsen05@gmail.com>
2022-10-03 17:22:44 +0300

    hamsi eklendi

---
+++ amadosya2.py
@@ -0,0 +1,19 @@
+soru=input("Azarbaycandan salam:")
+
+if soru=="Türkiyeden sucuk":
+    print("Çoh komih mal")
+elif soru=="Türkiyeden pastırma":
+    print("siz de bi bitmediniz")
+else:
+    print("Bu bir otomatik Robot chat mesajıdır, sohbetiniz bitmiştir!!")
+
+soru2=input("Naber:")
+
+if soru2=="Sanane" :
+    print("asdfasdlkfjş")
+elif soru2=="Kötü":
+    print("Kim kızdırdı küçük enişteyi")
+elif soru2=="Çok kötü":
+    print("Somon ayaklım benim")
+else:
+    print("Git hamsi ye hamsi")
